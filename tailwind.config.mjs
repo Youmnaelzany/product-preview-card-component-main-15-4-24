69a091ca2181d83183dd9035383b8b6eeba15c67
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2024-04-15 22:45:11 +0200

    setting the working space
    add project with astro
    add react & tailwind
    add file and folders
    install bun
    add prettier
    working with mobile first methods
    and now ready to deploy the project

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  prefix: "",
+  theme: {
+    fontFamily: {
+      Fraunces: ["Fraunces", "serif"],
+      Montserrat: ["Montserrat", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+};
